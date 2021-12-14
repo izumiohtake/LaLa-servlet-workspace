eecd4b764d3d73424be0348da1a6ae3f02c40992
izumiohtake <est88481025@gmail.com>
2021-12-14 16:32:52 +0900

    1214

---
+++ ex1214/src/servlet/SampleServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SampleServlet
+ */
+@WebServlet("/sample")//sampleでurl doGetで呼び出す
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//HTMLを出力
+	       response.setContentType("text/html; charset=UTF-8");
+	       PrintWriter out = response.getWriter();
+	       out.println("<html><body>");
+	       out.println("やっほー");
+	       out.println("</body></html>");
+	}
+}
---
+++ example/src/HelloServlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")//ウェブアノテーションおかしいと起動に失敗する
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("・・・");
+		out.println("</html>");
+		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+
+}
